dceaa313cc90f5e9f376e9e46864cb2306b3a385
mahmoudnourr <graphh37@gmail.com>
2024-05-16 21:50:32 +0300

    Adding Teacher Banner

---
+++ EducationalPlatform/DTO/TeacherBannerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace EducationalPlatform.DTO
+{
+	public class TeacherBannerDto
+	{
+	    public int Id { get; set; }
+		public string ProfileImageURl { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+
+	}
+}
